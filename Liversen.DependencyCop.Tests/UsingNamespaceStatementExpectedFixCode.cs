403352f9aa30d06626240cd414cceedbfb79997f
Morten Hartlev Lindhart <ykoksen@lindhart.com>
2024-07-12 08:44:24 +0200

    First expected code fix. Fixing transforming using statements into fully qualified usage.

---
+++ Liversen.DependencyCop.Tests/UsingNamespaceStatementExpectedFixCode.cs
@@ -0,0 +1,27 @@
+﻿
+namespace UsingNamespaceStatementAnalyzer.Account
+{
+    class Id
+    {
+        public string Value { get; set; }
+    }
+
+    class Item
+    {
+        public Id Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Transaction.Item[] Transactions { get; set; }
+    }
+}
+
+namespace UsingNamespaceStatementAnalyzer.Transaction
+{
+    class Item
+    {
+        public Account.Id AccountId { get; set; }
+
+        public string Text { get; set; }
+    }
+}
